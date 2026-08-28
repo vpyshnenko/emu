d6afd28d278113f59a640e5889625f3661ee3bbd
Viktor Pyshnenko <vik.pysh@gmail.com>
2026-08-28 12:45:54 +0300

    add utilits to ping_pong lib

--- examples/ping_pong/sim.ml
+++ examples/ping_pong/sim.ml
@@ -1,9 +1,11 @@
 
-
 let run n =
   let net = Netbuilder.attach_ext (Net.make_linear_net n) in
+  Utils.print_routing_map net;
+  (* let net = Netbuilder.attach_ext Net.cycle_net in *)
   let init_snap = Emu.Runtime.create net in
   Emu.Runtime.run init_snap ~schedule:[
+    (* { src = Netbuilder.ext_node_id; out_port = 0; payload = [n; 42] }; *)
     { src = Netbuilder.ext_node_id; out_port = 0; payload = [(n - 1); 42] };
     (* { src = Netbuilder.ext_node_id; out_port = 5; payload = [2; 42] }; *)
     (* { src = Netbuilder.ext_node_id; out_port = 4; payload = [8; 43] }; *)
